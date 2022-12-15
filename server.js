bc81f8a3524aaba6d179c28683048a0e9462a60f
Salih Yanbal <salixynbl@gmail.com>
2022-12-15 19:30:24 +0300

    forbuild

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const middlewareForFood = require('./middlewares/middlewareForFood')
 const menuRoute = require('./routes/menu')
 var bodyParser = require('body-parser')
@@ -8,11 +8,11 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Menu is ready to list menu')
+    res.statusCode(200).send('Menu is ready to list menu')
 })
 
 app.use('/menu', middlewareForFood, menuRoute)
 
 app.listen(port, () => {
-    console.log(`TodoAPP listening on port ${port}`)
+    console.log(`Menu APP listening on port ${port}`)
 })
